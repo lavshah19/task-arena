fd562d3d795501006fe4c517cb3a5e5c50d908bc
lav kush <lavshah1111@gmail.com>
2025-05-12 22:50:20 +0545

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // optional, for relative asset paths
+
   plugins: [react(), tailwindcss()],
 })
